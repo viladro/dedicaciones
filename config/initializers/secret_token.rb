3f1ec0cbe9ad4f742cf78e0b6ea777caa767cc4b
viladro@gmail.com <viladro@gmail.com>
2012-03-22 15:28:59 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testap::Application.config.secret_token = '1ddacbb7c3b4af5a020e8de8483f77bce0c7217278966b68bb012e933ed7f16715e22ef1a25ade42a62b33746285219ea6175e6586a0bf1d0a106fcc9e7c0223'
