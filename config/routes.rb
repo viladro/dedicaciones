1297eaabd2afc85da4e2d3a0bcd481d8b035f45b
viA <viladro@gmail.com>
2012-03-23 15:59:42 -0700

    3 demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Testap::Application.routes.draw do
+  resources :microposts
+
   resources :users
 
   # The priority is based upon order of creation:
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120323164506) do
+ActiveRecord::Schema.define(:version => 20120323223012) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
